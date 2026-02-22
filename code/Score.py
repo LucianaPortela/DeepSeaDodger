8b41b9ae60f494b3e3b5792a9703924c0d62c7c1
LucianaPB <Lucianabatista@outlook.com>
2026-02-22 17:21:19 -0300

    Initial commit Creating assets folder Creating Menu

---
+++ code/Score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Score:
+    def __init__(self):
+        self.window = None
+
+    def save(self, ):
+        pass
+
+    def show(self, ):
+        pass
